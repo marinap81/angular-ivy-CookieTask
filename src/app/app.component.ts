fa6c5a9ed1deeb6eae9ec11ee39884e3be956a26
marinap81 <marinapajvancic@hotmail.com>
2021-09-30 11:51:15 +1000

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,43 @@
- import { Component, OnInit, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 import { Colours } from './models/colours.enum';
 import { Cookie } from './models/cookie';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  implements OnInit{
- 
+export class AppComponent implements OnInit {
   //default cookies created
   selectCookie: Cookie;
-  cookieList: Cookie[] = [ // declared and initialized from  parent app.
+  cookieList: Cookie[] = [
+    // declared and initialized from  parent app.
     {
-      name: 'Marshmallow', colour: Colours.BROWN
+      name: 'Marshmallow',
+      colour: Colours.BROWN, chocchipNum: 0
     },
 
     {
-      name: 'Rockyroad', colour: Colours.BROWN
-    }
+      name: 'Rockyroad',
+      colour: Colours.BROWN, chocchipNum: 0
+    },
   ];
-
   
   //CREATING A NEW COOKIE
-  newCookiename: string;
-  //newCookieColour: Colours;
-  
+  newCookiename: string; 
+
+  //CHANGE COLOURS
+  selectColor: Colours;
+  colorList: Colours[] = [Colours.BLACK,Colours.BLUE, Colours.BROWN, Colours.PINK, Colours.WHITE];
+
 
   ngOnInit() {
     this.selectCookie = this.cookieList[0];
   }
-    
-    //function to CREATE NEW COOKIE 
-    addCookie(newCookiename: string) 
-    {
-      let cookie = new Cookie(newCookiename)
-      this.cookieList.push(cookie);}
-  
+
+  //function to CREATE NEW COOKIE
+  addCookie(newCookiename: string) {
+    let c = new Cookie(newCookiename);
+    this.cookieList.push(c);
+  }
 }
--- src/app/models/cookie.ts
+++ src/app/models/cookie.ts
@@ -3,12 +3,12 @@ import { Colours } from './colours.enum';
 export class Cookie {
   name: string;
   colour: Colours;
-  //chocchipNum: number;
+  chocchipNum: number;
 
   constructor(_name: string) {
     this.name = _name;
     this.colour = Colours.BROWN;
-    //this.chocchipNum = 0;
+    this.chocchipNum = 0;
 
   }
 }
